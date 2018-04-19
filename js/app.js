de15c0e0b9ab7d09ba729a579450a20d12a10e6d
Dmytro Chernyshov <dchertc@gmail.com>
2018-04-20 02:16:53 +0300

    Add script for hiding logo while scrolling down

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+/* Trigger the active state by appending class name is-active */
 $(document).ready(function(){
     $("#header-toggler").click(function(){
-        $(this).toggleClass("is-active");
+        $(this).toggleClass("is-active");       
     });
 });
+
+/* Hide logo while scrolling down */
+//When the user scrolls down 70px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+        document.getElementById("navbar-scroll").style.top = "-80px";
+    } else {
+        document.getElementById("navbar-scroll").style.top = "0";
+    }
+};
