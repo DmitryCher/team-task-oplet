9786d81ed9b5edbd2b6156128b2fcd6a7ac624f0
Dmytro Chernyshov <dchertc@gmail.com>
2018-04-24 16:06:26 +0300

    Add Scrollreveal library js file

--- js/app.js
+++ js/app.js
@@ -16,3 +16,50 @@ function scrollFunction() {
         document.getElementById("navbar-scroll").style.top = "0";
     }
 };
+
+/* ScrollReveal animations */
+window.sr = ScrollReveal({mobile: false});
+
+sr.reveal('.btn-outline-success', { 
+  origin: 'left',
+  distance: '200px', 
+  duration: 2500 
+});
+
+sr.reveal('.btn-outline-danger', { 
+  origin: 'right', 
+  distance: '200px',
+  duration: 2500 
+});
+
+sr.reveal('#prof figure',{ 
+  duration: 3000
+});
+
+sr.reveal('.administration figure',{   
+  duration: 3000
+});
+
+sr.reveal('.leftfield', { 
+  origin: 'left', 
+  distance: '200px',
+  duration: 2500 
+});
+
+sr.reveal('.rightfield', { 
+  origin: 'right', 
+  distance: '200px',
+  duration: 2500 
+});
+
+sr.reveal('.textfield', { 
+  origin: 'bottom', 
+  distance: '250px',
+  duration: 2500 
+});
+
+sr.reveal('.row .btn', { 
+  origin: 'bottom', 
+  distance: '200px',
+  duration: 3000 
+});
